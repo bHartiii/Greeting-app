c055ba2e7f8d063589b3a1c28515197ec99c204f
malibharti5@gmail.com <malibharti5@gmail.com>
2020-12-19 01:36:02 +0530

    Bharti-ADD : Delete warning is added before deleting card

--- greeting_app/static/js/greeting.js
+++ greeting_app/static/js/greeting.js
@@ -1,12 +1,26 @@
 function validateform(){  
-    var name=document.form.name.value;  
-    var msg=document.form.msg.value;  
-      
-    if (name==null || name==""){  
-      alert("Name can't be blank!");  
-      return false;  
-    }else if(msg==null|| msg==""){  
-      alert("Messgage can't be blank!");  
-      return false;  
-      }  
+  var name=document.form.name.value;  
+  var msg=document.form.msg.value;  
+    
+  if (name==null || name==""){  
+    alert("Name can't be blank!");  
+    return false;  
+  }else if(msg==null|| msg==""){  
+    alert("Messgage can't be blank!");  
+    return false;  
     }  
+  }
+
+  $(document).ready(function() {
+    $(".btn-outline-danger").click(function() {
+      var id = $(this).attr("id");
+      var check = confirm("Are you sure you want to delete ?" );
+      if(check==true){
+        window.location.href = id;
+      }else{
+        return false;
+      }
+    });
+  });
+  
+ 
